3daf5c5e358c5b3fb6540580c39783cb74b4eb90
Abhishek Bhardwaj <64359785+iabhishekbhardwaj@users.noreply.github.com>
2022-04-20 20:13:35 +0530

    Add files via upload

---
+++ login_page/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-TONO6RK;Initial Catalog=DATA_NEW;Integrated Security=True;Pooling=False;");
+            SqlDataAdapter sda = new SqlDataAdapter
+
+            main ss = new main();
+            ss.Show();
+
+        }
+    }
+}
